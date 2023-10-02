564b58c58f2193bfdc603ed4a070a3fb4c7b7048
David Pallarés <serallapdivad@gmail.com>
2023-10-02 11:52:04 +0100

    Added booking reducer to the store so the whole app is aware if the changes

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import bookingSlice from '../features/bookings/bookingSlice'
 
 export const store = configureStore({
     reducer: {
+        bookings: bookingSlice
     },
 })
