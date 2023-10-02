25ad3a63ee86543b5cb2a02acce7c73965e557fa
David Pallarés <serallapdivad@gmail.com>
2023-10-02 22:45:11 +0100

    Created user slice

---
+++ src/features/users/userSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { fetchInitialUsers } from './userThunks'
+
+
+const initialState = {
+    initialUserFetch: [],
+    status: 'idle',
+    error: 'null'
+}
+
+const userSlice = createSlice({
+    name: 'users',
+    initialState,
+    reducers: {},
+    extraReducers: builder => {
+        builder
+            .addCase(fetchInitialUsers.pending, (state, action) => {
+                state.status = 'pending'
+            })
+            .addCase(fetchInitialUsers.rejected, (state, action) => {
+                state.status = 'rejected'
+            })
+            .addCase(fetchInitialUsers.fulfilled, (state, action) => {
+                state.initialUserFetch = action.payload
+                console.log(state.initialUserFetch)
+                state.status = 'fulfilled'
+            })
+    }
+})
+
+export default userSlice.reducer
+
+export const initialUsers = state => state.users.initialUserFetch
+export const fetchRoomState = state => state.users.status
