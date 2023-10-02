acaf01dd068a7552cb4cf0e803e546baf1d1a772
David Pallarés <serallapdivad@gmail.com>
2023-10-02 22:45:44 +0100

    Added users reducer to the store

--- src/app/store.js
+++ src/app/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import bookingSlice from '../features/bookings/bookingSlice'
 import contactSlice from '../features/contact/contactSlice'
 import roomSlice from '../features/rooms/roomSlice'
+import userSlice from '../features/users/userSlice'
 
 
 export const store = configureStore({
@@ -9,5 +10,6 @@ export const store = configureStore({
         bookings: bookingSlice,
         contacts: contactSlice,
         rooms: roomSlice,
+        users: userSlice,
     },
 })
