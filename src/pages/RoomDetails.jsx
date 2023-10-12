75a36aea3de905853218404e32294d5550c0e01d
David Pallarés <serallapdivad@gmail.com>
2023-10-12 13:55:33 +0100

    Improved styles on swiper nav buttons

--- src/pages/RoomDetails.jsx
+++ src/pages/RoomDetails.jsx
@@ -11,7 +11,6 @@ import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 import 'swiper/css/scrollbar'
-import './room_details_styles.css'
 
 const RoomDetails = () => {
 	const navigate = useNavigate()
