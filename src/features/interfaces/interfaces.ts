6abb2ede39f98b2fd5ec9eae6febd60f94157bd3
David Pallarés <serallapdivad@gmail.com>
2023-10-17 23:58:05 +0100

    Created interfaces for: - Bookings - Rooms - User - Contact

--- src/features/interfaces/interfaces.ts
+++ src/features/interfaces/interfaces.ts
@@ -11,3 +11,40 @@ export interface BookingInter {
 	status: string
 	photos: string[]
 }
+
+export interface RoomInter {
+	room_number: string
+	id: string
+	room_photo: string[]
+	room_type: string
+	description: string
+	amenities_type: string
+	amenities: object[]
+	price: number
+	offer_price: boolean
+	discount: number
+	status: string
+}
+
+export interface ContactInter {
+	id: string
+	full_name: string
+	email: string
+	phone_number: string
+	subject_of_review: string
+	review_body: string
+	date: string
+	dateTime: string
+	isArchived: string
+}
+
+export interface UserInter {
+	employee_id: string
+	full_name: string
+	email: string
+	photo: string
+	start_date: string
+	description: string
+	phone_number: string
+	status: string
+}
