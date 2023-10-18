66d8ca41d66ffa70c9414ac63807167b3f29061d
David Pallarés <serallapdivad@gmail.com>
2023-10-18 13:25:57 +0100

    - Rename: "vite.config.js" - With: "vite.config.ts"

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(() => {
-    return {
-        build: {
-            outDir: 'build',
-        },
-        plugins: [react()],
-    }
+	return {
+		build: {
+			outDir: 'build',
+		},
+		plugins: [react()],
+	}
 })
