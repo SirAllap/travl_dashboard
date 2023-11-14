5901f634506c2f7aeeb2b60ee154835a1b154413
David PR <serallapdivad@gmail.com>
2023-11-14 13:14:24 +0000

    Generated a dinamic method to fetch data from API

---
+++ src/util/fetchMethod.ts
@@ -0,0 +1,29 @@
+const API_local_URL = import.meta.env.VITE_API_LOCAL_URL
+
+export const fetchMethod = async (
+	returData: boolean,
+	URLendpoint: string,
+	method: string,
+	id?: string,
+	body?: any
+) => {
+	const response = await fetch(`${API_local_URL}/${URLendpoint}`, {
+		method: method,
+		mode: 'cors',
+		headers: {
+			'Content-Type': 'application/json',
+			token: `${localStorage.getItem('token')}`,
+		},
+		body: JSON.stringify(body),
+	})
+	if (!response.ok) {
+		throw new Error(`status ${response.status}`)
+	} else {
+		if (returData) {
+			const data = await response.json()
+			return data
+		} else {
+			return id
+		}
+	}
+}
